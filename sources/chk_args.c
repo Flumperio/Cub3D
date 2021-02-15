08e30b2e0f1783f55cd8c94855166084d84bb83f
Juan Carlos <juasanto@student.42madrid.com>
2021-02-15 18:15:45 +0100

    15 - feb -home

--- sources/chk_args.c
+++ sources/chk_args.c
@@ -6,25 +6,50 @@
 /*   By: juasanto <juasanto@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 11:20:01 by juasanto          #+#    #+#             */
-/*   Updated: 2021/02/12 11:42:50 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/02/15 18:14:25 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cube3d.h"
+
+int	chk_name(char *name)
+{
+	char	*test_ext;
+
+	test_ext = &name[(ft_strlen(name) - 4)];
+	if(ft_strcmp(test_ext, ".cub") == 0)
+		return (0);
+	return (3);
+}
+
+int	chk_save(char *save)
+{
+	if(ft_strcmp(save, "--save") == 0)
+		return (0);
+	return (4);
+}
+
+
 
 int	chk_args(int argc, char **argv)
 {
 	int		cnt;
 
 	cnt = 0;
-	if (argc <= 1)
-		return (1);
-	if (argc > 3)
-		return (2);
+	/* While para comprobar los argumentos, Borrar */
 	while (cnt < argc)
 	{
 		printf("N.Args: %i -- Argumento: %i --- Parametro: %s\n", argc, cnt, argv[cnt]);
 		cnt++;
 	}
+	/* Borrar While */
+	if (argc <= 1)
+		return (1);
+	if (argc > 3)
+		return (2);
+	if (chk_name(argv[1]) != 0)
+		return (3);
+	if (argv[2] && chk_save(argv[2]) != 0)
+		return (4);
 	return (0);
 }
--- sources/cube3d.c
+++ sources/cube3d.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/12 10:22:37 by juasanto          #+#    #+#             */
-/*   Updated: 2021/02/15 12:59:34 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/02/15 17:48:51 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,20 @@
 
 int		main(int argc, char **argv)
 {
-	char	*error_msg[]= { "Error. No hay parametros", "Error. Demasiados parámetros" };
+	char	*error_msg[4];
 	int		chk;
 
-	chk = 0;
+	error_msg[0] = "Error. No hay parametros";
+	error_msg[1] = "Error. Demasiados parámetros";
+	error_msg[2] = "Error. La extensión del archivo no es correcta";
+	error_msg[3] = "Error. El parametro no es correcto.";
 	chk = chk_args(argc, argv);
-	printf("Valor de chk: %i\n", chk);
+	printf("valor chk: %i\n", chk);
 	if (chk != 0)
 	{
-		printf("%s", error_msg[chk - 1]);
+		printf("%s\n", error_msg[chk - 1]);
 		return (chk);
 	}
-	printf("OK");
+	printf("OK\n");
 	return (0);
 }
