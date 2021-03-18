58d2964ad8c30106aafc4413cab31cdc14cdc28d
Juan Carlos <juasanto@student.42madrid.com>
2021-03-18 13:29:54 +0100

    18 - Mar

--- sources/wrk_map.c
+++ sources/wrk_map.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/10 12:07:10 by juasanto          #+#    #+#             */
-/*   Updated: 2021/03/16 14:01:00 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/03/18 13:26:12 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,18 +60,13 @@ void	cnvrt_map(t_cube *s_c3d)
 
 	cnt = 0;
 	cnt1 = 11;
-	m_val = ft_strdup("012NSWE ");
+	m_val = ft_strdup("012NSWE \t");
 	while (s_c3d->line[cnt] != '\0')
 	{
 		if (ft_isblank(s_c3d->line[cnt]))
 			s_c3d->line[cnt] = '0';
-		while (cnt1 > 0)
-		{
-			if (s_c3d->line[cnt] == m_val[cnt1])
-				cnt1--;
-			else
-				ft_msgerror ("No valid map.", 7);
-		}
+		if (!ft_strchr(m_val, s_c3d->line[cnt]))
+			ft_msgerror ("No valid map.", 7);
 		cnt++;
 	}
 	s_c3d->wrk_map[s_c3d->cnt_map++] = ft_strdup(s_c3d->line);
