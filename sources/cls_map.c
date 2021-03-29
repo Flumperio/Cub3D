f0a4a30a36ca299d4f15657e07a49913abf763b0
Juan Carlos <flumperio@gmail.com>
2021-03-29 18:43:32 +0200

    29 - Mar - Home

--- sources/cls_map.c
+++ sources/cls_map.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/25 13:10:40 by juasanto          #+#    #+#             */
-/*   Updated: 2021/03/29 14:11:02 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/03/29 18:31:25 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,89 +14,18 @@
 
 int	cls_map(t_cube *cub, t_map *map, int x, int y)
 {
-	// int	c_x;
-	// int	c_y;
-	// int	c_l;
-	// //int c_x1, c_y1;
-	// //int	l_len;
-
-	// c_x = x;
-	// c_y = y;
-	// c_l = cub->map_lines;
-	//cub->tmp = 1;
-	//map->temp = 0;
-	printf("PL: %c, x: %i - y: %i\n", cub->pl_view, x, y);
-	if (x == 0 || y == 0 || y == ft_strlen(cub->tmp_map[x]) - 1 || cub->tmp_map[x + 1][y] == '\0'||
-		y > ft_strlen(cub->tmp_map[x - 1]) || y > ft_strlen(cub->tmp_map[x + 1]))
+	printf("PL: %c, x: %i - y: %i - MapLines: %i\n", cub->pl_view, x, y, cub->cnt_map);
+	if (x == 0 || y == 0 || y == ft_strlen(cub->tmp_map[x]) - 1 || x == cub->cnt_map - 1 || \
+		y > ft_strlen(cub->tmp_map[x + 1]))
 		ft_msgerror("ABIERTO", 6);
 	cub->tmp_map[x][y] = '3';
 	if (cub->tmp_map[x - 1][y] == '0')
-	{
 		cls_map(cub, map, x - 1, y);
-	}
 	if (cub->tmp_map[x][y + 1] == '0')
-	{
 		cls_map(cub, map, x, y + 1);
-	}
 	if (cub->tmp_map[x + 1][y] == '0')
-	{
 		cls_map(cub, map, x + 1, y);
-	}
 	if (cub->tmp_map[x][y - 1] == '0')
-	{
 		cls_map(cub, map, x, y - 1);
-	}
-
-	// if (cub->tmp_map[x][y + 1] == '0')
-	// {
-	// 	cub->tmp_map[x][y + 1] = '3';
-	// 	y++;
-	// 	cls_map(cub, map);
-	// }
-	// if (cub->tmp_map[x + 1][y] == '0')
-	// {
-	// 	cub->tmp_map[x + 1][y] = '3';
-	// 	x++;
-	// 	cls_map(cub, map);
-	// }
-	// if (cub->tmp_map[x][y - 1] == '0')
-	// {
-	// 	cub->tmp_map[x][y - 1] = '3';
-	// 	y--;
-	// 	cls_map(cub, map);
-	// }
-	// if (cub->tmp_map[x][y + 1] == '0')
-	// {
-	// 	cub->tmp_map[x][y + 1] = '3';
-	// 	y++;
-	// 	cls_map(cub, map);
-	// }
-
-	// c_x1 = 0;
-	// c_y1 = 0;
-	// while (cub->tmp_map[c_x1] != 0)
-	// {
-	// 	while (cub->tmp_map[c_x1][c_y1] != 0)
-	// 	{
-	// 		printf("%c", cub->tmp_map[c_x1][c_y1]);
-	// 		c_y1++;
-	// 	}
-	// 	printf("\n");
-	// 	c_y1 = 0;
-	// 	c_x1++;
-	// }
 	return (0);
 }
-
-	// while (cnt_x < cub->cnt_map)
-	// {
-	// 	l_len = ft_strlen(cub->tmp_map[cnt_x]);
-	// 	while (cnt_y < l_len)
-	// 	{
-	// 		printf("%c", cub->tmp_map[cnt_x][cnt_y]);
-	// 		cnt_y++;
-	// 	}
-	// 	printf("\n");
-	// 	cnt_y = 0;
-	// 	cnt_x++;
-	// }
--- sources/cube3d.c
+++ sources/cube3d.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/26 10:31:45 by juasanto          #+#    #+#             */
-/*   Updated: 2021/03/29 13:35:38 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/03/29 18:35:10 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,18 +55,18 @@ int	main(int argc, char **argv)
 	init_map(&cub, map);
 	tmp_map(&cub, map);
 	cub.tmp = 0;
-	cls_map(&cub, map, cub.pl_posx, cub.pl_posy);
-	//system("leaks cub3D");
-	while (cub.wrk_map[tmp])
-	{
-		printf("map_line: %s\n", cub.wrk_map[tmp]);
-		tmp++;
-	}
 	tmp = 0;
 	printf("\n\n");
 	while (cub.tmp_map[tmp])
 	{
 		printf ("tmp_line: %s\n", cub.tmp_map[tmp]);
+		tmp++;
+	}
+	cls_map(&cub, map, cub.pl_posx, cub.pl_posy);
+	//system("leaks cub3D");
+	while (cub.wrk_map[tmp])
+	{
+		printf("map_line: %s\n", cub.wrk_map[tmp]);
 		tmp++;
 	}
 	tmp = 0;
