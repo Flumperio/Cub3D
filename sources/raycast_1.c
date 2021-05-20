2a93992129109a61ba0b99fad7c7f81782f4116d
Juan Carlos <flumperio@gmail.com>
2021-05-20 16:04:58 +0200

    20 - May

--- sources/raycast_1.c
+++ sources/raycast_1.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/30 19:07:38 by juasanto          #+#    #+#             */
-/*   Updated: 2021/05/18 09:55:35 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/05/20 15:32:49 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,6 +32,8 @@ int	raycast_loop(t_cube *cub)
 	}
 	sprites_print(cub);
 	mlx_put_image_to_window(cub->mlx.mlx, cub->mlx.mlx_win, cub->mlx.img, 0, 0);
+	if (cub->save_parm == 1)
+		main_bmp(cub);
 	mlx_destroy_image(cub->mlx.mlx, cub->mlx.img);
 	return (0);
 }
@@ -41,8 +43,8 @@ void	raycast(t_cube *cub)
 	init_ray(cub);
 	stg_tex(cub);
 	sprites(cub);
-	if (cub->save_parm == 1)
-		main_bmp(cub);
+	// if (cub->save_parm == 1)
+	// 	main_bmp(cub);
 	mlx_hook(cub->mlx.mlx_win, 2, 1L << 0, key_press, cub);
 	mlx_hook(cub->mlx.mlx_win, 3, 1L << 1, key_relea, cub);
 	mlx_hook(cub->mlx.mlx_win, 17, 1L << 17, ui_cross_exit, cub);
--- sources/screen_capture.c
+++ sources/screen_capture.c
@@ -6,7 +6,7 @@
 /*   By: juasanto <juasanto>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/17 17:24:18 by juasanto          #+#    #+#             */
-/*   Updated: 2021/05/18 16:21:14 by juasanto         ###   ########.fr       */
+/*   Updated: 2021/05/20 16:04:09 by juasanto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,18 +18,21 @@ void	main_bmp(t_cube *cub)
 	int	ret;
 	int	value;
 	int	tmp;
+	int	cnt;
 
-
+	cnt = 0;
 	printf("Take ScreenShot\n");
+	system("pkill afplay");
 	fd = open("screen_shot.bmp", O_RDWR | O_CREAT, S_IRUSR | S_IWUSR);
 	printf("FD: %i\n", fd);
 	ret = write(fd, "BM", sizeof(char) * 2);
 	value = ((cub->resX * cub->resY) * 4) + 14 + 40 ;
 	printf ("v: %i\n", value);/* tamaño del fichero * 3 uso 24bpp */
-	ret = write(fd, &value, sizeof(unsigned int));
+	ret = write(fd, &value, 4);
 	value = 0;
-	ret = write(fd, &value, sizeof(unsigned short int));
-	ret = write(fd, &value, sizeof(unsigned short int));
+	ret = write(fd, &value, 1);
+	ret = write(fd, &value, 1);
+	ret = write(fd, &value, 2);
 	value = 54;
 	ret = write(fd, &value, 4);
 	value = 40;
@@ -40,7 +43,7 @@ void	main_bmp(t_cube *cub)
 	ret = write(fd, &value, 4);
 	value = 1;
 	ret = write(fd, &value, 2);
-	value = 24;
+	value = 32;
 	ret = write(fd, &value, 2);
 	value = 0;
 	ret = write(fd, &value, 4);
@@ -49,13 +52,12 @@ void	main_bmp(t_cube *cub)
 	ret = write(fd, &value, 4);
 	ret = write(fd, &value, 4);
 	ret = write(fd, &value, 4);
-	tmp = ((cub->resX * cub->resY) * 3);
-	while (tmp < 0)
+	tmp = ((cub->resX * cub->resY) * 4);
+	while (cnt < tmp)
 	{
-		ret = write (fd, &value, 3);
-		tmp--;
+		write (fd, &cub->mlx.addr[cnt], 1);
+		cnt++;
 	}
-
 	close(fd);
 	free_all(cub);
 	exit(0);
